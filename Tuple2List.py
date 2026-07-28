ca479ee4ddab6345df1cd00f59d448d9a167c6c7
Muzammil Sait <saitmuzz007@gmail.com>
2026-07-28 14:46:32 +0530

    Add Tuple2List.py to convert tuple to list

---
+++ Tuple2List.py
@@ -0,0 +1,12 @@
+colors = ("Red", "Blue", "Green", "Yellow")
+
+print("Original Tuple:", colors)
+
+color_list = list(colors)
+
+print("Converted List:", color_list)
+
+color_list.append("Purple")
+color_list.remove("Blue")
+
+print("Updated List:", color_list)
